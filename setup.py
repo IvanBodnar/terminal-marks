5b99995c998bd64543684ddeebcb34af9ca24621
ivan <ibodnar@outlook.com>
2020-06-25 21:30:00 -0300

    setup.py: package_dir and find_packages(where=src)

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='python-template',
     version='0.1',
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     entry_points='''
         [console_scripts]
         run=src.main:run

31b3d2f6644f6aab538525bd46df3305dc53f3c0
ivan <ibodnar@outlook.com>
2020-06-26 17:25:53 -0300

    get_input fn tested in cli

--- src/terminal_marks/cli/cli.py
+++ src/terminal_marks/cli/cli.py
@@ -1,7 +1,13 @@
 import click
+import os
+
+from terminal_marks.api import get_input
 
 
 @click.command()
 @click.argument('mark', type=click.STRING)
 def run(mark):
-    click.echo(mark)
+    i = get_input()
+    i.mark_name = mark
+    i.read_cwd()
+    print(i.mark_name, i.directory.as_posix())

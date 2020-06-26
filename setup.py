e33b66a6fdd1eed49319129af4f0973e6db72b67
ivan <ibodnar@outlook.com>
2020-06-26 15:09:36 -0300

    setup.py: install_requires click

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='python-template',
     version='0.1',
+    install_requires=[
+            'click',
+        ],
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     entry_points='''
         [console_scripts]
-        run=src.main:run
+        run=terminal_marks.cli.cli:run
     ''',
 )
